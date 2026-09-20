a5f7f8ea431e2162c28f1a757876f26d5e3ef844
Aadyant <aadyantgaming5@gmail.com>
2026-09-20 18:22:59 +0530

    Made Navbar restructed the file structure

---
+++ dashboard/app/dashboard/guild/[guildId]/analytics/page.tsx
@@ -0,0 +1,5 @@
+"use client";
+
+import { useeffect, useState } from "react";
+import { useParams } from "next/navigation";
+import { Card, CardHeader, CArdTitle, CardDescription, CardContent } from "@/components/ui/card";
